4aba8f206c6aaa1c16b9ef5d668dcecd9b2629b1
Josh <peterson.joshua.j@gmail.com>
2017-04-08 22:36:59 -0400

    Installed CmdMessenger and updated the GUI

--- GUI/userInterface.py
+++ GUI/userInterface.py
@@ -1,8 +1,28 @@
 from tkinter import *
+#from pynput import ket,listener
 
 root = Tk()
+
+letter = ""
+
 label = Label(root, text="test a letter")
 label.pack(side = LEFT)
+
 input = Entry()
 input.pack(side = RIGHT)
+
+def submitLetter():
+    letter = input.get()
+    #while TRUE:
+
+
+
+submit = Button(text = "submit", command = submitLetter)
+submit.pack()
+
+
+
 root.mainloop()
+
+
+
